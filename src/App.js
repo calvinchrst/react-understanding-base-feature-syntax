c24d45cfee9257be1864dae707de94cb68237042
Calvin Christian <calvinchrst@gmail.com>
2020-05-03 17:22:47 +0800

    Revert App to using class state instead of useState hook

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-const app = (props) => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: "Max", age: 20 },
       { name: "Marta", age: 29 },
       { name: "Calvin", age: 24 },
     ],
-  });
+  };
 
-  const [otherState, setOtherState] = useState("Just some other states");
-
-  console.log(personsState, otherState);
-
-  const switchNameHandler = (newName) => {
-    setPersonsState({
+  switchNameHandler = () => {
+    this.setState({
       persons: [
-        { name: newName, age: 20 },
+        { name: "Maximillian", age: 20 },
         { name: "Marta", age: 29 },
         { name: "Calvin", age: 25 },
       ],
     });
   };
 
-  const changeNameHandler = (event) => {
-    setPersonsState({
+  changeNameHandler = (event) => {
+    this.setState({
       persons: [
         { name: "Max", age: 20 },
         { name: event.target.value, age: 29 },
@@ -35,7 +31,7 @@ const app = (props) => {
     });
   };
 
-  const style = {
+  style = {
     backgroundColor: "white",
     font: "inherit",
     border: "1px solid blue",
@@ -43,34 +39,33 @@ const app = (props) => {
     cursor: "pointer",
   };
 
-  return (
-    <div className="App">
-      <h1>Hello Everyone!</h1>
-      <p>This is really working!</p>
-      <button
-        style={style}
-        onClick={switchNameHandler.bind(this, "Maximillian!")}
-      >
-        Switch Name
-      </button>
-      <Person
-        name={personsState.persons[0].name}
-        age={personsState.persons[0].age}
-      />
-      <Person
-        name={personsState.persons[1].name}
-        age={personsState.persons[1].age}
-        click={switchNameHandler.bind(this, "Max!")}
-        change={changeNameHandler}
-      >
-        My hobby is sleeping :p
-      </Person>
-      <Person
-        name={personsState.persons[2].name}
-        age={personsState.persons[2].age}
-      />
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello Everyone!</h1>
+        <p>This is really working!</p>
+        <button style={this.style} onClick={this.switchNameHandler}>
+          Switch Name
+        </button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "Max!")}
+          change={this.changeNameHandler}
+        >
+          My hobby is sleeping :p
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+  }
+}
 
-export default app;
+export default App;
