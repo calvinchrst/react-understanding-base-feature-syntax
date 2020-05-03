5054b995f7116cf0dde1b7d460745954f446c337
Calvin Christian <calvinchrst@gmail.com>
2020-05-03 17:33:41 +0800

    Handling dynamic content the javascript way

--- src/App.js
+++ src/App.js
@@ -48,6 +48,30 @@ class App extends Component {
   };
 
   render() {
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, "Max!")}
+            change={this.changeNameHandler}
+          >
+            My hobby is sleeping :p
+          </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hello Everyone!</h1>
@@ -58,26 +82,7 @@ class App extends Component {
         <button style={this.style} onClick={this.switchNameHandler}>
           Switch Name
         </button>
-        {this.state.showPersons ? (
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-            />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, "Max!")}
-              change={this.changeNameHandler}
-            >
-              My hobby is sleeping :p
-            </Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-            />
-          </div>
-        ) : null}
+        {persons}
       </div>
     );
   }
