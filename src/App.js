225e0e9629af5ce1465ea85f4d5028bfc7150ee4
Calvin Christian <calvinchrst@gmail.com>
2020-05-05 10:58:42 +0800

    Using Styled Components package

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Radium, { StyleRoot } from "radium";
 
 import "./App.css";
 import Person from "./Person/Person";
@@ -53,10 +52,10 @@ class App extends Component {
       border: "1px solid blue",
       padding: "8px",
       cursor: "pointer",
-      ":hover": {
-        backgroundColor: "lightgreen",
-        color: "black",
-      },
+      // ":hover": {
+      //   backgroundColor: "lightgreen",
+      //   color: "black",
+      // },
     };
 
     const classes = [];
@@ -93,18 +92,16 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
-        <div className="App">
-          <h1>Hello Everyone!</h1>
-          <p className={classes.join(" ")}>This is really working!</p>
-          <button style={style} onClick={this.togglePersonsHandler}>
-            Toggle Persons
-          </button>
-          {persons}
-        </div>
-      </StyleRoot>
+      <div className="App">
+        <h1>Hello Everyone!</h1>
+        <p className={classes.join(" ")}>This is really working!</p>
+        <button style={style} onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </button>
+        {persons}
+      </div>
     );
   }
 }
 
-export default Radium(App);
+export default App;
