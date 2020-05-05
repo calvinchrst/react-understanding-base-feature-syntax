a4afcd8194b514ce68508d2913257b26e3f217b7
Calvin Christian <calvinchrst@gmail.com>
2020-05-05 11:18:18 +0800

    Dynamically styling button using styled components package

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import "./App.css";
 import Person from "./Person/Person";
 
 const StyledButton = styled.button`
-  background-color: green;
+  background-color: ${(props) => (props.alt ? "red" : "green")};
   color: white;
   font: inherit;
   border: 1px solid blue;
   padding: 8px;
   cursor: pointer;
+
   &:hover {
-    background-color: lightgreen;
+    background-color: ${(props) => (props.alt ? "salmon" : "lightgreen")};
     color: black;
   }
 `;
@@ -84,19 +85,16 @@ class App extends Component {
           })}
         </div>
       );
-
-      // style.backgroundColor = "red";
-      // style[":hover"] = {
-      //   backgroundColor: "salmon",
-      //   color: "black",
-      // };
     }
 
     return (
       <div className="App">
         <h1>Hello Everyone!</h1>
         <p className={classes.join(" ")}>This is really working!</p>
-        <StyledButton onClick={this.togglePersonsHandler}>
+        <StyledButton
+          alt={this.state.showPersons}
+          onClick={this.togglePersonsHandler}
+        >
           Toggle Persons
         </StyledButton>
         {persons}
