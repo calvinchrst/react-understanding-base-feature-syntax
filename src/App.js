fb8950d7dbaf7687f92f1084107b6d9531204a90
Calvin Christian <calvinchrst@gmail.com>
2020-05-07 12:19:05 +0800

    Revert "Using Error Boundaries"
    This reverts commit 585148d8022251fe46d920685bf09e58b60607d1.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 
 import classes from "./App.css";
 import Person from "./Person/Person";
-import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
 
 class App extends Component {
   state = {
@@ -61,14 +60,13 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <ErrorBoundary key={person.id}>
-                <Person
-                  name={person.name}
-                  age={person.age}
-                  click={this.deletePersonHandler.bind(this, index)}
-                  change={(event) => this.changeNameHandler(event, person.id)}
-                />
-              </ErrorBoundary>
+              <Person
+                name={person.name}
+                age={person.age}
+                click={this.deletePersonHandler.bind(this, index)}
+                change={(event) => this.changeNameHandler(event, person.id)}
+                key={person.id}
+              />
             );
           })}
         </div>
