f6c0345ba317f0d5f9f87d8763bf6c3d90cd4b9c
Calvin Christian <calvinchrst@gmail.com>
2020-05-08 11:59:47 +0800

    Adding console.log to Component Update Lifecycle methods for props changes. Some functional components are converted to class to see the lifecycle methods

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Person from "./Person/Person";
 
-const persons = (props) => {
-  console.log("[Persons.js] rendering...");
-  return props.persons.map((person, index) => {
-    return (
-      <Person
-        name={person.name}
-        age={person.age}
-        clicked={() => props.clicked(index)}
-        changed={(event) => props.changed(event, person.id)}
-        key={person.id}
-      />
-    );
-  });
-};
+class Persons extends Component {
+  state = {
+    test: true,
+  };
 
-export default persons;
+  static getDerivedStateFromProps(props, state) {
+    console.log("[Persons.js] getDerivedStateFromProps");
+    return state;
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[Persons.js] shouldComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("[Persons.js] getSnapshotBeforeUpdate");
+    return { message: "Snapshot!" };
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("[Persons.js] componentDidUpdate");
+    console.log(snapshot);
+  }
+
+  render() {
+    console.log("[Persons.js] rendering...");
+    return this.props.persons.map((person, index) => {
+      return (
+        <Person
+          name={person.name}
+          age={person.age}
+          clicked={() => this.props.clicked(index)}
+          changed={(event) => this.props.changed(event, person.id)}
+          key={person.id}
+        />
+      );
+    });
+  }
+}
+
+export default Persons;
