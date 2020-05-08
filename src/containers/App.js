dd2e1bb5b970ad48d42eb64bd514f5083d54ddc9
Calvin Christian <calvinchrst@gmail.com>
2020-05-08 11:43:36 +0800

    Add console log to all Component Creation Lifecycle methods

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,11 @@ import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("[App.js] constructor");
+  }
+
   state = {
     persons: [
       { id: "asdsad12", name: "Max", age: 20 },
@@ -13,6 +18,15 @@ class App extends Component {
     ],
     showPersons: false,
   };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("[App.js] getDerivedStateFromProps", props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log("[App.js] componentDidMount");
+  }
 
   changeNameHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex((p) => {
@@ -46,6 +60,7 @@ class App extends Component {
   };
 
   render() {
+    console.log("[App.js] render");
     let persons = null;
     if (this.state.showPersons) {
       persons = (
