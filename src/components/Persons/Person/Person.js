3d2450c38957994b2209c5baa2e380a5dc6e5d3a
Calvin Christian <calvinchrst@gmail.com>
2020-05-09 14:55:33 +0800

    Using Auxiliary HOC as workaround to return JSX with adjacent root elements

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,22 +1,26 @@
 import React, { Component } from "react";
 
 import classes from "./Person.css";
+import Auxiliary from "../../../hoc/Auxiliary";
 
 class Person extends Component {
   render() {
     console.log("[Person.js] rendering...");
     return (
-      <div className={classes.Person}>
-        <p onClick={this.props.clicked}>
-          I'm a {this.props.name} and I am {this.props.age} years old!
-        </p>
-        <p>{this.props.children}</p>
-        <input
-          type="text"
-          onChange={this.props.changed}
-          value={this.props.name}
-        />
-      </div>
+      <Auxiliary>
+        <div className={classes.Person}>
+          <p onClick={this.props.clicked}>
+            I'm a {this.props.name} and I am {this.props.age} years old!
+          </p>
+          <p>{this.props.children}</p>
+          <input
+            type="text"
+            onChange={this.props.changed}
+            value={this.props.name}
+          />
+        </div>
+        <div></div>
+      </Auxiliary>
     );
   }
 }
